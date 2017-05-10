ad25e999c8d3f19207384b502bbba90e6b0d0c42
Daniel J. Vera <djvera@gmail.com>
2017-05-10 12:32:27 -0700

    Add files via upload

--- r4ds/r4ds/r4ds_ch10.R
+++ r4ds/r4ds/r4ds_ch10.R
@@ -244,10 +244,170 @@ ggplot(flights_avg, aes(lon, lat)) +
                
 # 2. Add the location of the origin and destination (i.e. the lat and lon)
 # to flights.
-
+flights %>% 
+  left_join(airports, by = c("origin" = "faa")) %>%
+  left_join(airports, by = c("dest" = "faa")) %>%
+  select(year:time_hour, lat.x, lon.x, lat.y, lon.y)
+  
 # 3. Is there a relationship between the age of a plane and its delays?
+planes_age <- planes %>%
+  mutate(age = 2013 - year)
+
+flights %>%
+  left_join(planes_age, by = "tailnum") %>%
+  group_by(tailnum) %>%
+  mutate(avg_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  ggplot(aes(age, avg_delay)) +
+  geom_point() +
+  geom_smooth()
+
+# jrnold solution:
+flights %>%
+  inner_join(planes_age, by = "tailnum") %>%
+  group_by(age) %>%
+  filter(!is.na(arr_delay)) %>%
+  summarize(delay = mean(arr_delay)) %>%
+  ggplot(aes(age, delay)) +
+  geom_point() +
+  geom_line()
+
+# both solutions show little relationship between age and delay, surprisngly.
 
 # 4. What weather conditions make it more likely to see a delay?
+flights_weather <- flights %>%
+  inner_join(weather, c(
+    "year" = "year",
+    "month" = "month",
+    "day" = "day",
+    "hour" = "hour"
+  ))
+
+flights_weather %>%
+  group_by(precip) %>%
+  summarize(delay = mean(dep_delay, na.rm = TRUE)) %>%
+  ggplot(aes(precip, delay)) +
+  geom_line() +
+  geom_point()
 
 # 5. What happened on June 13 2013? Display the spatial pattern of delays, 
 # and then use Google to cross-reference with the weather.
+library(viridis)
+flights %>% 
+  filter(year == 2013, month == 6, day == 13) %>%
+  group_by(dest) %>%
+  summarize(delay = mean(arr_delay, na.rm = TRUE)) %>%
+  inner_join(airports, by = c("dest" = "faa")) %>%
+  ggplot(aes(lon, lat, size = delay, color = delay)) +
+  borders("state") +
+  geom_point() + 
+  coord_quickmap() + 
+  scale_color_viridis()
+
+# according to https://www.ncdc.noaa.gov/sotc/national/201306, tropical 
+# storm Andrea made landfall on June 6, in Florida.
+
+# Filtering Joins ==========================================================
+top_dest <- flights %>%
+  count(dest, sort = TRUE) %>%
+  head(10)
+
+flights %>%
+  filter(dest %in% top_dest$dest)
+
+flights %>%
+  semi_join(top_dest)
+
+flights %>%
+  anti_join(planes, by = "tailnum") %>%
+  count(tailnum, sort = TRUE)
+
+# Exercises 13.5.1 on website:
+# http://r4ds.had.co.nz/tidy-data.html#exercises-29
+# 1. What does it mean for a flight to have a missing tailnum? 
+# What do the tail numbers that don’t have a matching record in planes 
+# have in common? (Hint: one variable explains ~90% of the problems.)
+missing_tailnum <- flights %>%
+  filter(is.na(tailnum))
+# it appears none of these flights took place, they were probably cancelled.
+no_planes_record <- flights %>%
+  anti_join(planes, by = "tailnum")
+
+no_planes_record %>%
+  count(carrier, sort = TRUE)
+
+# MQ and AA have the most missing tail numbers. 
+
+# 2. Filter flights to only show flights with planes that have flown at 
+# least 100 flights.
+at_least_100_flights <- flights %>%
+  count(tailnum) %>%
+  filter(n > 100)
+
+flights %>%
+  semi_join(at_least_100_flights, by = "tailnum")
+
+# 3. Combine fueleconomy::vehicles and fueleconomy::common to find only 
+# the records for the most common models.
+fueleconomy::vehicles
+fueleconomy::common
+
+semi_join(fueleconomy::vehicles, 
+          fueleconomy::common, 
+          by = c("make", "model")
+          )
+
+# 4. Find the 48 hours (over the course of the whole year) that have the 
+# worst delays. Cross-reference it with the weather data. Can you see any 
+# patterns?
+
+flights %>%
+  mutate(dep_hour = dep_time %/% 100) %>%
+  group_by(dep_hour) %>%
+  summarize(avg_delay = mean(dep_delay, na.rm = TRUE)) %>%
+  arrange(desc(avg_delay))
+# this gives the worst hour being 3 am but the question asks for worst 48
+# hours so it must be looking for the top 48 most delayed flights?
+worst48 <- flights %>%
+  arrange(desc(dep_delay)) %>%
+  head(48)
+
+# 5. What does 
+anti_join(flights, airports, by = c("dest" = "faa")) 
+# tell you? What does 
+anti_join(airports, flights, by = c("faa" = "dest")) 
+# tell you?
+
+# The first gives the flights that do not have flights the airports FAA
+# list. The second gives the airports that have no flight data in this table
+# i.e. there were no flights from New York to these airports in 2013.
+
+# 6. You might expect that there’s an implicit relationship between plane 
+# and airline, because each plane is flown by a single airline. 
+# Confirm or reject this hypothesis using the tools you’ve learned above.
+
+flights %>%
+  group_by(tailnum, carrier) %>%
+  count() %>%
+  filter(n() > 1) %>% 
+  select(tailnum) %>%
+  distinct()
+
+# Join Problems ============================================================
+airports %>% count(alt, lon) %>% filter(n > 1)
+
+# Set Operations ===========================================================
+df1 <- tribble( 
+  ~ x, ~ y, 
+    1,   1, 
+    2,   1 
+  ) 
+df2 <- tribble( 
+  ~ x, ~ y, 
+    1,   1, 
+    1,   2 
+  )
+
+intersect(df1, df2)
+union(df1, df2)
+setdiff(df1, df2)
+setdiff(df2, df1)
